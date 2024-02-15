3a1f6713dd2057d4c772e5a9b50b4280abddab20
roger437unix <33252885+roger437unix@users.noreply.github.com>
2024-02-14 22:20:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 '''
 --> Criar Docker container MySQL <--
 
+docker network create --subnet 192.168.0.0/24 tux
+
 docker run -d --rm --name=mysql \
 -v $PWD/dados:/var/lib/mysql \
 -p 3306:3306 \
+--net tux \
+--ip 192.168.0.100 \
 -e MYSQL_ROOT_PASSWORD=mysql \
 -e MYSQL_ROOT_HOST=% \
 -e MYSQL_DATABASE=db_users \
@@ -12,7 +16,7 @@ docker run -d --rm --name=mysql \
 mysql \
 --default-authentication-plugin=mysql_native_password
 
-mariadb -utux -p -h 127.0.0.1 db_users
+mariadb -utux -p -h 192.168.0.100 db_users
 
 '''
 
@@ -20,8 +24,8 @@ mariadb -utux -p -h 127.0.0.1 db_users
 
 from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine, text
-import os
 from credenciais import db_config
+import os
 
 app = Flask(__name__)
 
@@ -31,36 +35,46 @@ url  = db_config['host']
 db   = db_config['database']
 
 
-# Connect to the database
-engine = create_engine(f"mysql+mysqlconnector://{user}:{pw}@{url}/{db}")    
-connection = engine.connect()
+def conexao():
+    engine = create_engine(f"mysql+mysqlconnector://{user}:{pw}@{url}/{db}")    
+    connection = engine.connect()
+    return connection
 
-query_create_table = '''
-CREATE TABLE IF NOT EXISTS users (
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(50) NOT NULL,
-fone VARCHAR(50) NOT NULL,
-email VARCHAR(50) NOT NULL
-);
-'''
-connection.execute(text(f"{query_create_table}"))
+
+def tabela():
+    query_create_table = '''
+    CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    fone VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+    );
+    '''
+    connection = conexao()
+    connection.execute(text(f"{query_create_table}"))
+    connection.close()
 
 
 def select():
     global lista
     lista = []
-    result = connection.execute(text("SELECT * FROM users"))         
+    connection = conexao()
+    result = connection.execute(text("SELECT * FROM users"))
+    connection.close()        
     for row in result:
-        lista.append(row)
-    # print(f'Select ==> {lista}')
+        lista.append(row)    
 
 
 lista = []
+tabela()
 select()
 
 
 @app.get("/")
-def home():       
+def home():        
+    ref = request.args.get('ref')   
+    if ref == '1':
+        select()           
     return render_template("base.html", lista_front=lista)
 
 
@@ -69,7 +83,7 @@ def add():
     user = []    
     nome  = request.form.get("nome")
     fone  = request.form.get("fone")
-    email = request.form.get("email")
+    email = request.form.get("email")    
 
     if nome != '' and fone != '' and email != '':        
         user.append(nome)
@@ -80,31 +94,33 @@ def add():
             query_insert = f"INSERT INTO users (nome, fone, email) \
             VALUES ('{nome}', '{fone}', '{email}')"
 
+            connection = conexao()
             connection.execute(text(query_insert))
             connection.execute(text("COMMIT"))
-            select()  
+            connection.close()              
         except:
             print('Falha no "insert"')                             
     else:
-        print('** Usuário não cadastrato, todos os dados devem ser fornecidos **')    
-    return redirect(url_for("home"))
+        print('** Usuário não cadastrato, todos os dados devem ser fornecidos **')        
+    return redirect(url_for("home", ref=1))
 
 
 @app.post("/sort")
 def sort():
-    if lista != []:
-        print(f'** Ordenando a lista **')
+    # ref = False
+    if lista != []:        
+        print(f'** Ordenando a lista **')        
         lista.sort(key=lambda x: x[1])       
-    return redirect(url_for("home"))
+    return redirect(url_for("home", ref=0))
 
 
 @app.post("/reverse")
 def reverse():
     global lista
-    if lista != []:
+    if lista != []:        
         print(f'** Invertendo a lista **')        
         lista = sorted(lista, reverse=True, key=lambda x: x[1])
-    return redirect(url_for("home"))
+    return redirect(url_for("home", ref=0))
 
 
 @app.post("/clear")
@@ -112,16 +128,17 @@ def clear():
     print(f'==> Apagando todos registros do banco de dados <==')
     lista_id = []
     try:
+        connection = conexao()
         result = connection.execute(text("SELECT id FROM users"))         
         for row in result:
             lista_id.append(row[0])
         for id in lista_id:
             connection.execute(text(f"DELETE FROM users WHERE id = {id}"))
-        connection.execute(text("COMMIT"))    
-        select()
+        connection.execute(text("COMMIT"))
+        connection.close()        
     except:
-        print('Falha em "limpar tabela"')
-    return redirect(url_for("home"))
+        print('Falha em "limpar tabela"')    
+    return redirect(url_for("home", ref=1))
 
 
 @app.get("/delete/<id_banco>")
@@ -129,12 +146,13 @@ def delete(id_banco):
     id = id_banco
     print(f'==> Removendo Id: {id}')
     try:
+        connection = conexao()
         connection.execute(text(f"DELETE FROM users WHERE id = {id}"))
         connection.execute(text("COMMIT"))
-        select()
+        connection.close()        
     except:
-        print('Falha ao excluir usuário')         
-    return redirect(url_for("home"))
+        print('Falha ao excluir usuário')             
+    return redirect(url_for("home", ref=1))
 
 
 if __name__ == '__main__':    
