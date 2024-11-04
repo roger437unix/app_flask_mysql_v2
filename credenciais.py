3cdaa8287903daa03eea8725b92d65fe3128b8c4
ROGÉRIO DA SILVA ALCANTARA <sn1024493@CFP127ALUNO.LOCAL>
2024-11-04 07:49:15 -0300

    first commit

--- credenciais.py
+++ credenciais.py
@@ -1,6 +1,20 @@
-db_config = {
-  'host': '127.0.0.1',
-  'user': 'tux',
-  'password': 'ABC123xyz',
-  'database': 'db_users'
-}
+import os, sys
+
+USER     = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+HOST     = os.getenv('HOST')
+
+if USER is not None and PASSWORD is not None and HOST is not None:
+  db_config = {
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
+    'database': 'db_users'
+  }
+else:
+  print(f'\n*** The enviroment variables "USER", "PASSWORD" and "HOST" are empty ***\n')
+  print(f'Execute no Git Bash:\n')
+  print(f'export USER     = "<DATABASE_USER>"')
+  print(f'export PASSWORD = "<DATABASE_USER_PASSWORD>"')
+  print(f'export HOST     = "<DATABASE_ADDRESS>"\n')
+  sys.exit()
