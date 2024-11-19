9a744c90c27e019f7cb89266a1ec6834998e5682
roger437unix <33252885+roger437unix@users.noreply.github.com>
2024-11-19 17:26:05 -0300

    Update credenciais.py

--- credenciais.py
+++ credenciais.py
@@ -1,20 +1,20 @@
 import os, sys
 
-USER     = os.getenv('USER')
-PASSWORD = os.getenv('PASSWORD')
-HOST     = os.getenv('HOST')
+USER = os.getenv('USER')
+PASS = os.getenv('PASS')
+HOST = os.getenv('HOST')
 
-if USER is not None and PASSWORD is not None and HOST is not None:
+if USER is not None and PASS is not None and HOST is not None:
   db_config = {
     'host': HOST,
     'user': USER,
-    'password': PASSWORD,
+    'password': PASS,
     'database': 'db_users'
   }
 else:
-  print(f'\n*** The enviroment variables "USER", "PASSWORD" and "HOST" are empty ***\n')
+  print(f'\n*** The enviroment variables "USER", "PASS" and "HOST" are empty ***\n')
   print(f'Execute no terminal:\n')
-  print(f'export USER     = "<DATABASE_USER>"')
-  print(f'export PASSWORD = "<DATABASE_USER_PASSWORD>"')
-  print(f'export HOST     = "<DATABASE_ADDRESS>"\n')
-  sys.exit()
+  print(f'export USER = "<DATABASE_USER>"')
+  print(f'export PASS = "<DATABASE_USER_PASSWORD>"')
+  print(f'export HOST = "<DATABASE_ADDRESS>"\n')
+  sys.exit()  
